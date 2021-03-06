669953e9ba564db2e430cb8e7b5afd432dc98c2d
Allie <Allie122209@gmail.com>
2021-03-06 15:28:19 -0500

    database tables 2

---
+++ final-capstone/java/database/prizes.sql
@@ -0,0 +1,14 @@
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS prizes;
+CREATE TABLE prizes(
+    prize_id serial PRIMARY KEY,
+    family_id INTEGER NOT NULL,
+    prize_name varchar NOT NULL, --Did not add unique constraint as the same prize name could be added more than once...But is this what we want? 
+    minutes_needed int NOT NULL,
+    max_awarded int NOT NULL, --Do we want not null here? What if it's a small prize always available?
+    awarded int NOT NULL, --Date format should be yyyy-mm-dd by default, so we will need to account for that in how it interacts with the date formatter on the front end.
+
+    CONSTRAINT fk_family_id FOREIGN KEY (family_id) REFERENCES families (family_id),
+    
+);
