344fc48c656ffd6bd8e18881397a6f75731e45be
Allie <Allie122209@gmail.com>
2021-03-08 11:24:02 -0500

    fixed primary keys

--- final-capstone/java/database/create_table_people.sql
+++ final-capstone/java/database/create_table_people.sql
@@ -4,7 +4,7 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS people;
 
 CREATE TABLE people(
-        people_id SERIAL NOT NULL,
+        people_id SERIAL PRIMARY KEY NOT NULL,
         first_name varchar(50) NOT NULL,
         last_name varchar(50)
         
