00b14d7583174423972eb2ddfdbcc059cbc0e7d0
Jan Cooper <Jan.Cooper@live.com>
2021-03-11 19:30:29 -0500

    add book and user to books_users table when creating a book; write test for same

--- final-capstone/java/src/main/java/com/techelevator/dao/BookDAO.java
+++ final-capstone/java/src/main/java/com/techelevator/dao/BookDAO.java
@@ -5,8 +5,7 @@ import java.util.List;
 import com.techelevator.model.Book;
 
 public interface BookDAO {
-	public boolean createBook(String author, String isbn, String title, String imgLink);
-public List<Book> listAll();
-
-public Book getBookByID(int book_id);
+	public boolean createBook(String author, String isbn, String title, String imgLink, int currentUserId);
+	public List<Book> listAll();
+	public Book getBookByID(int book_id);
 }
--- final-capstone/java/src/test/java/com/techelevator/dao/BookSqlDaoIntegrationTest.java
+++ final-capstone/java/src/test/java/com/techelevator/dao/BookSqlDaoIntegrationTest.java
@@ -1,47 +1,86 @@
 package com.techelevator.dao;
 
-import com.techelevator.model.Book;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import javax.sql.DataSource;
-import javax.swing.Action;
 
 public class BookSqlDaoIntegrationTest extends DAOIntegrationTest {
 
-    private BookSqlDAO BookSqlDAO;
+    private BookSqlDAO bookSqlDAO;
+    private JdbcTemplate jdbcTemplate;
     private String MESSAGE;
     private String ISBN;
     private String TITLE;
     private String AUTHOR;
     private String IMG;
+    private int USER_ID;
 
     @Before
     public void setup() {
         DataSource dataSource = this.getDataSource();
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        BookSqlDAO = new BookSqlDAO(jdbcTemplate);
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        bookSqlDAO = new BookSqlDAO(jdbcTemplate);
     }
 
     @Test
-    public void createNewBook(){
+    public void createNewBookReturnsTrue(){
         // arrange 
     	MESSAGE = "Creating a new book returns true.";
         ISBN = "isbn";
         TITLE = "book_title";
         AUTHOR = "author_name";
         IMG = "img_link";
+        USER_ID = 1;
         boolean expected = true;
 
         // act
-        boolean actual = BookSqlDAO.createBook(AUTHOR, ISBN, TITLE, IMG);
+        boolean actual = bookSqlDAO.createBook(AUTHOR, ISBN, TITLE, IMG, USER_ID);
 
         // assert
         assertEquals(expected, actual, MESSAGE);
+    }
+    
+    @Test
+    public void createNewBookUpdatesBooksUsersTable(){
+        // arrange 
+        String userIdSql = "SELECT user_id from users where username = ?";
+        SqlRowSet userIdResult = jdbcTemplate.queryForRowSet(userIdSql, "family_reading_tracker_appuser");
+        int user_id = 0;
+        if(userIdResult.next()) {
+        	user_id = userIdResult.getInt("user_id");
+        }
+        
+        ISBN = "isbn";
+        TITLE = "book_title";
+        AUTHOR = "author_name";
+        IMG = "img_link";
+        bookSqlDAO.createBook(AUTHOR, ISBN, TITLE, IMG, user_id);               
+        
+        String BookIdSql = "SELECT book_id FROM books WHERE title = ? AND author = ? AND isbn = ?";
+        SqlRowSet bookIdResult = jdbcTemplate.queryForRowSet(BookIdSql, TITLE, AUTHOR, ISBN);
+        int bookId = 0;
+        if(bookIdResult.next()) {
+        	bookId = bookIdResult.getInt("book_id");
+        }
+
+        // act
+        String bookUserSql = "SELECT books_users_id, book_id, user_id FROM books_users WHERE book_id = ? AND user_id = ?";
+        SqlRowSet booksUsersResult = jdbcTemplate.queryForRowSet(bookUserSql, bookId, user_id);
+        int bookUserId = 0;
+        if(booksUsersResult.next()) {
+        	bookUserId = booksUsersResult.getInt("books_users_id");
+        }
+        
+        // assert
+        assertNotEquals(0, bookUserId);
+        assertNotNull(bookUserId);
     }
 
 }
