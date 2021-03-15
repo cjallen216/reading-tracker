b16ac18f777b42ea56d1debfb42f81e451355685
Allie <Allie122209@gmail.com>
2021-03-15 11:03:49 -0400

    added dashboard in components

---
+++ final-capstone/vue/src/components/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+name:'dashboard'
+}
+</script>
+
+<style>
+.home{
+
+
+
+display: grid;
+grid-template-columns: 1fr 1fr 1fr 1fr;
+gap: 15px;
+grid-template-areas:
+
+"hours-read hours-read current-read current-read"
+"running-clock running-clock start-stop-button start-stop-button" 
+"books-read books-read next-achieve next achieve"
+"new-book new-book wish-list wish-list";
+
+align-items: center;
+
+}
+</style>
