66efecd60e3c4b89926454f25850ba53eebdb418
Chris Allen <christopherjallen216@gmail.com>
2021-03-17 10:49:54 -0400

    removed unnecessary code from timer, added this.total to make function work

--- final-capstone/vue/src/components/Timer.vue
+++ final-capstone/vue/src/components/Timer.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="timer">
-    <div>{{formattedElapsedTime}}</div>
-    <button class="button" @click="start" v-if="!this.isRunning">Start Timer</button>
-    <button class="button" @click="stop" v-if="this.isRunning">Stop Timer</button>
+    <div>{{ formattedElapsedTime }}</div>
+    <button class="button" @click="start" v-if="!this.isRunning">
+      Start Timer
+    </button>
+    <button class="button" @click="stop" v-if="this.isRunning">
+      Stop Timer
+    </button>
     <button class="button" @click="reset">Reset</button>
-    <h4>Total Time Spent Reading: {{this.total}}</h4>
-    <h4>Total Time Spent Reading: {{$store.state.total}}</h4>
-  
-  
+    <h4>Total Time Spent Reading: {{ $store.state.total }}</h4>
   </div>
 </template>
 
@@ -19,7 +20,7 @@ export default {
       elapsedTime: 0,
       timer: undefined,
       isRunning: false,
-      total: 0
+      total: 0,
     };
   },
   computed: {
@@ -30,14 +31,11 @@ export default {
       return utc.substr(utc.indexOf(":") - 2, 8);
     },
     formattedTotal() {
-       
-
-
       const date = new Date(null);
       date.setSeconds(this.total / 1000);
       const utc = date.toUTCString();
       return utc.substr(utc.indexOf(":") - 2, 8);
-    }
+    },
   },
   methods: {
     start() {
@@ -49,34 +47,29 @@ export default {
     stop() {
       clearInterval(this.timer);
       this.isRunning = false;
-      total = this.total + this.timer;
-      
+      this.total = this.total + this.timer;
     },
     reset() {
-        this.total = this.total + this.elapsedTime
-        this.$store.commit('SAVE_TIME', this.formattedTotal)
-       this.elapsedTime = 0;
-          
+      this.total = this.total + this.elapsedTime;
+      this.$store.commit("SAVE_TIME", this.formattedTotal);
+      this.elapsedTime = 0;
     },
-  }
+  },
 };
 </script>
 
 <style>
-
 #timer {
-    display: flex;
-    flex-direction: column;
-    align-content: center;
-    width: 250px;
-    gap: 10px;
-    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-    font-size: 4rem;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  width: 250px;
+  gap: 10px;
+  font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+  font-size: 4rem;
 }
 
 h4 {
-    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-    font-size: 2rem;
+  font-size: 2rem;
 }
-
 </style>
